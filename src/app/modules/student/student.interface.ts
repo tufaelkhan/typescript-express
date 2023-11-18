de9697f1244db471154986735c1f6bbfde46ccd3
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-18 11:16:33 -0800

    add type

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,28 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type Guardian = {
+        fatherName: string;
+        fatherOccupation: string;
+        fatherContactNo: string;
+        motherName: string;
+        motherOccupation: string;
+        motherContactNo: string;
+}
+
+export type Student = {
+    id: string;
+    name: {
+        firstName: string;
+        middleName: string;
+        lastName: string;
+    },
+    gender: "male" | "female";
+    dateOfBirth: string;
+    email: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    bloodGroup?: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+    presentAddress: string;
+    permanentAddress: string;
+    guardian: Guardian;
+  }
