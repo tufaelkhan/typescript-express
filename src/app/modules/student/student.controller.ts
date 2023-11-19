a7d5287afce82f65c39d0c3641b7366c8e749317
tufaelkhan <tufaelkhan247@gmail.com>
2023-11-19 09:17:42 -0800

    add controller

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,7 +1,20 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+import { StudentServices } from './student.service';
 
-const createStudent = (req: Request, res: Response) => {
+const createStudent = async (req: Request, res: Response) => {
+  try {
     const student = req.body;
-    // will call service func to send this data
-    // send response
+    const result = await StudentServices.createStudentIntoDB(student);
+    res.status(200).json({
+      success: true,
+      message: 'student is created successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const StudentControllers = {
+    createStudent,
 }
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -3,4 +3,9 @@ import { StudentModel } from "./student.model";
 
 const createStudentIntoDB = async(student: Student) => {
     const result = await StudentModel.create(student)
+    return result;
+}
+
+export const StudentServices = {
+    createStudentIntoDB,
 }
