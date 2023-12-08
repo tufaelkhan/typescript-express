627828a8a65cc4c341d3675a7ee99e4d0de071b7
tufaelkhan <tufaelkhan247@gmail.com>
2023-12-08 02:08:35 -0800

    add router

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { StudentRoutes } from './app/modules/student/student.route';
 import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middleWares/global.errorHandler';
 import notFound from './app/middleWares/notFound';
+import router from './app/routes';
 const app: Application = express();
 
 // parsers
@@ -13,14 +14,13 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
-const getAController = (req: Request, res: Response) => {
+const testPurpose = (req: Request, res: Response) => {
   res.send('ph university welcome');
 };
 
-app.get('/', getAController);
+app.get('/', testPurpose);
 
 app.use(globalErrorHandler)
 
